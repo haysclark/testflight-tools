239e7c63618888661596cd93c7fe41c0a197209f
Hays Clark <hays@infinitedescent.com>
2013-12-08 15:58:58 -0800

    Adding first stab at adding shunit2 tests and having Travis-CI handle testing.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 testflight-tools
 ================
+[![Build Status](https://travis-ci.org/haysclark/testflight-tools.png)](https://travis-ci.org/haysclark/testflight-tools)
 
 A un-official command line tool that simplifies uploading builds to TestFlight
---
+++ test/helper.sh
@@ -0,0 +1,12 @@
+[[ -z "$SHUNIT2"     ]] && SHUNIT2=/usr/share/shunit2/shunit2
+[[ -n "$ZSH_VERSION" ]] && setopt shwordsplit
+
+export PREFIX="$PWD/test"
+export HOME="$PREFIX/home"
+export PATH="$PWD/bin:$PATH"
+
+test_path="$PATH"
+
+setUp() { return; }
+tearDown() { return; }
+oneTimeTearDown() { return; }
